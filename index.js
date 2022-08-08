952eec818b6ae3ce589b4ec90b62b3c35b42ee8a
park_so_young <soyoung931014@gmail.com>
2022-08-09 03:25:46 +0900

    ADD: readOnly 값 변경

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ readOnly: appConfig.readOnly });
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
