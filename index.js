554f7d42f0b5b53d80bc0b8c50e878773e46937f
park_so_young <soyoung931014@gmail.com>
2022-08-09 00:26:34 +0900

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+// Add Express
+const express = require('express');
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get('/', (req, res) => {
+  res.send('Express on Vercel');
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log('Running on port 5000.');
+});
+
+module.exports = app;
