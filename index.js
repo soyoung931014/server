11e37a11b7ac390b4a0809d4a3770c3a1195308e
park_so_young <soyoung931014@gmail.com>
2022-08-09 02:43:39 +0900

    ADD: json-server로 바꿈

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-// Add Express
-const express = require("express");
-const hotels = require("./api/hotels");
-const reserved = require("./api/reservation");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
-const cors = require("cors");
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
 
-// Initialize Express
-const app = express();
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
 
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel 🚀");
+server.use(router);
+
+server.listen(5000, () => {
+  console.log(`JSON Server is running, port(${5000})`);
 });
-app.use("/hotels", hotels);
-app.use("/reserved", reserved);
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// /* eslint-disable no-undef */
+// // Add Express
+// const express = require("express");
+// const hotels = require("./api/hotels");
+// const reserved = require("./api/reservation");
 
-app.use(cors());
+// const cors = require("cors");
 
-// Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
+// // Initialize Express
+// const app = express();
 
-module.exports = app;
+// // Create GET request
+// app.get("/", (req, res) => {
+//   res.send("Express on Vercel 🚀");
+// });
+// app.use("/hotels", hotels);
+// app.use("/reserved", reserved);
+
+// app.use(cors());
+
+// // Initialize server
+// app.listen(5000, () => {
+//   console.log("Running on port 5000.");
+// });
+
+// module.exports = app;
